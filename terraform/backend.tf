50b34f5a59c611724f7f94f38652e1c59b48a415
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-05-03 17:39:56 +0200

    fix: backend initialisation

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "azurerm" {
-    subscription_id      = "1e702451-e125-465b-ba86-4343891daaa8"
-    resource_group_name  = "rg-prism-cluster-shared"
-    storage_account_name = "saprismclustershared"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
