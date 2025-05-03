9d447258592fd9bd63b60ee3fee1797b8d95f28f
mallory.brickerd <mallory.brickerd@eraneos.com>
2025-05-03 17:59:24 +0200

    fix: Terraform version

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    subscription_id      = "1e702451-e125-465b-ba86-4343891daaa8"
+    resource_group_name  = "rg-prism-cluster-shared"
+    storage_account_name = "saprismclustershared"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
